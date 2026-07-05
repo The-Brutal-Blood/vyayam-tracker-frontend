313e747ff5661e2336758f19f63c250e803627ce
Hemant-Programming <hemant.kashyap@programming.com>
2026-07-05 16:29:44 +0530

    chore: initial React Native project setup

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Vyayam Tracker 💪</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
